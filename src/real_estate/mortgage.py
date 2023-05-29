3f02a3ba45385a06f824dbabdb38099cfec1a3af
rdodkins <rupertdodkins@gmail.com>
2023-05-28 17:06:11 -0700

    mortgage insuranc

--- src/real_estate/mortgage.py
+++ src/real_estate/mortgage.py
@@ -2,18 +2,19 @@ import numpy as np
 import pandas as pd
 
 class Mortgage():
-    def __init__(self, yearly_interest, loan_amount, total_years=30):
+    def __init__(self, yearly_interest, loan_amount, mort_insur_frac=0.01, total_years=30):
         self.yearly_interest = yearly_interest
         self.total_years = total_years
         self.loan_amount = loan_amount
+        self.mort_insur_frac = mort_insur_frac
         
         # self.yearly_interest /= 100
         self.monthly_interest = self.yearly_interest / 12
         self.num_payments = total_years * 12
-        self.monthly_PI = self.monthly_PI()
+        self.monthly_PI = self.calc_monthly_PI()
         self.df = self.amortization_df()
     
-    def monthly_PI(self):
+    def calc_monthly_PI(self):
         monthly_PI = self.loan_amount * (self.monthly_interest * np.power(1 + self.monthly_interest, self.num_payments)) / (np.power(1 + self.monthly_interest, self.num_payments) - 1)
         return monthly_PI
     
@@ -37,11 +38,18 @@ class Mortgage():
         # Subtract to get the principal for each payment
         principal_payment = self.monthly_PI - interest_payment
 
+        # Compute monthly mortgage insurance payment
+        mortgage_insurance_payment = np.where((self.loan_amount - np.cumsum(principal_payment)) > 0.8 * self.loan_amount, self.loan_amount * self.mort_insur_frac / 12, 0)
+        
+        # Add mortgage insurance to monthly payment
+        total_monthly_payment = self.monthly_PI + mortgage_insurance_payment
+
         # Put these arrays into a DataFrame
         data = {
-            'Payment': np.full(self.num_payments, self.monthly_PI),
+            'Payment': total_monthly_payment,
             'Principal': principal_payment,
             'Interest': interest_payment,
+            'Mortgage Insurance': mortgage_insurance_payment,
             'Remaining Balance': remaining_balance
         }
         amortization_schedule = pd.DataFrame(data)
